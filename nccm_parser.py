232b91d17726750037cb77c31baf1ce92ff2148d
mortvest <dmitry.serykh@gmail.com>
2017-12-24 01:58:25 +0100

    terminal interface added

--- nccm_parser.py
+++ nccm_parser.py
@@ -2,13 +2,16 @@ from html.parser import HTMLParser
 import datetime
 import re
 import requests
+import argparse
 
 WEEKDAYS = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"]
-CANTEENS = ["August Krogh", "Biocenter"]
+CANTEEN_NAMES = ["Biocenter", "August Krogh"]
 PAGE_URLS = ["http://www.biocenter.ku.dk/kantine/menuoversigt/",
              "http://www1.bio.ku.dk/akb/kantine/menuoversigt/"]
-# TODO: Add terminal interface
-TODAY = datetime.date.today().weekday()
+TODAY = datetime.date.today().weekday() - 3
+# TODAY = datetime.date.today().weekday()
+# TODO Add support for printing tables
+# TODO Add error handlers
 
 class MyHTMLParser(HTMLParser):
     lst = []
@@ -34,11 +37,10 @@ def get_for_a_day(date, dishes):
 def get_for_a_canteen(canteen, dishes):
     return [x for x in dishes if (x.canteen == canteen)]
 
-
 def print_for_day(day,lst):
     dishes = get_for_a_day(lst,day)
     print("* " + dishes[0].date)
-    for canteen in CANTEENS:
+    for canteen in CANTEEN_NAMES:
         foo = get_for_a_canteen(canteen, dishes)
         print("** " + foo[0].canteen)
         for dish in foo:
@@ -55,18 +57,16 @@ def print_for_today(dishes):
     print_for_day(dishes,weekday)
 
 def load_list_for_canteen(url, pool, canteen_name):
-    r = requests.get(url)
     parser = MyHTMLParser()
+    r = requests.get(url)
     page = "\n".join(re.findall("^.*tr height=.*$", r.text, re.MULTILINE))
     # page = ""
-    with open('test1.html', 'r') as myfile:
-        page = myfile.read()
+    # with open('test1.html', 'r') as myfile:
+    #     page = myfile.read()
     parser.feed(page)
     var = parser.get_list()
     if len(var) < 25:
         print("ABORTED, " + canteen_name + " - menu inclomlete")
-    elif TODAY > 4:
-        print("ABORTED, " + canteen_name + " - the canteens are closed today!")
     else:
         for j in range(5):
             date = re.match(r"(.*) (.*) - (.*)", var.pop(0), flags=0).group(3)
@@ -77,8 +77,22 @@ def load_list_for_canteen(url, pool, canteen_name):
             dish_name = var.pop(0)
             pool.append(Dish(dish_type, dish_name, date, canteen_name))
 
-pool = []
-for nr in range(len(CANTEENS)):
-    load_list_for_canteen(PAGE_URLS[nr], pool, CANTEENS[nr])
-# print_for_today(pool)
-print_for_week(pool)
+def load_all():
+    pool = []
+    for nr in range(len(CANTEEN_NAMES)):
+        load_list_for_canteen(PAGE_URLS[nr], pool, CANTEEN_NAMES[nr])
+    return pool
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-w", "--week", help="show menu for week and exit",
+                    action="store_true")
+group.add_argument("-t", "--today", help="show menu for today and exit (default)",
+                   action="store_true")
+
+args = parser.parse_args()
+if args.week:
+    print_for_week(load_all())
+else:
+    print_for_today(load_all())
+# TODO add support for choosing canteens and days
